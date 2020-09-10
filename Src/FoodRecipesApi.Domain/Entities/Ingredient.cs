19ce3a6992fa8c6674aef1a1a1aa5bb9555fc572
Daniel Ras <ras98.daniel@gmail.com>
2020-09-10 20:10:25 +0300

    feat: add Ingredient domain entity

---
+++ Src/FoodRecipesApi.Domain/Entities/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Domain.Entities
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string Name { get; set; }
+        public IngredientQuantity Quantity {get; set;}
+    }
+}
