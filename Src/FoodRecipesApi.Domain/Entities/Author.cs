02324c0fec8980d4853633dd87ca142ae39a6374
Daniel Ras <ras98.daniel@gmail.com>
2020-09-10 20:40:31 +0300

    feat: add Author domain entity

---
+++ Src/FoodRecipesApi.Domain/Entities/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Domain.Entities
+{
+    public class Author
+    {
+        public Author()
+        {
+            Recipes = new List<Recipe>();
+        }
+
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Born { get; set; }
+        public TimeSpan Age => Born - DateTime.Now;
+        public ICollection<Recipe> Recipes { get; private set; }
+    }
+}
