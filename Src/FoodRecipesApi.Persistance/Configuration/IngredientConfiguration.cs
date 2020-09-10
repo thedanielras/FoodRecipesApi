202c47bbb293673a386081c59683dd6dd1787276
Daniel Ras <ras98.daniel@gmail.com>
2020-09-11 00:48:41 +0300

    feat: add ef FluentApi Ingredient Configuration class

---
+++ Src/FoodRecipesApi.Persistance/Configuration/IngredientConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using FoodRecipesApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Persistence.Configuration
+{
+    class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
+    {
+        public void Configure(EntityTypeBuilder<Ingredient> builder)
+        {
+            builder.HasOne(i => i.Quantity)
+               .WithOne(q => q.Ingredient)
+               .HasForeignKey<Ingredient>(i => i.QuantityId)
+               .IsRequired();
+
+            builder.Property(i => i.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
