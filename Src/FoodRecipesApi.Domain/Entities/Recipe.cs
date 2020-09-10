da367f384deaa342badf34428495a60d2ceffd9b
Daniel Ras <ras98.daniel@gmail.com>
2020-09-11 00:45:11 +0300

    feat: add RecipeIngredient ICollection property

--- Src/FoodRecipesApi.Domain/Entities/Recipe.cs
+++ Src/FoodRecipesApi.Domain/Entities/Recipe.cs
@@ -15,10 +15,11 @@ namespace FoodRecipesApi.Domain.Entities
         public string Title { get; set; }
         public string Description { get; set; }
         public Author Author { get; set; }
+        public int AuthorId { get; set; }
         public ICollection<RecipeStep> RecipeSteps { get; private set; }
         public string ImageUrl { get; set; }
         public TimeSpan PreparationTime { get; set; }
         public TimeSpan TotalTime { get; set; }
-        public ICollection<Ingredient> Ingredients { get; set; }
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; }
     }
 }
