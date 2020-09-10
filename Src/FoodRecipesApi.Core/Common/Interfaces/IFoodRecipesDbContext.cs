ae6a8d8713fa530ee3c7df79d0fdbfeafc0ea0d7
Daniel Ras <ras98.daniel@gmail.com>
2020-09-10 20:33:34 +0300

    feat: add IFoodRecipesDbContext interface

---
+++ Src/FoodRecipesApi.Core/Common/Interfaces/IFoodRecipesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using FoodRecipesApi.Domain.Entities;
+
+namespace FoodRecipesApi.Application.Common.Interfaces
+{
+    interface IFoodRecipesDbContext
+    {
+        DbSet<Recipe> Recipes { get; set; }
+    }
+}
