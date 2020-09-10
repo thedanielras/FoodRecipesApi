1450588ba7a1548f1e495c5f7b752f9dea7b6e40
Daniel Ras <ras98.daniel@gmail.com>
2020-09-11 00:49:51 +0300

    feat: add ef FluentApi RecipeStep Configuration class

---
+++ Src/FoodRecipesApi.Persistance/Configuration/RecipeStepConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FoodRecipesApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Persistence.Configuration
+{
+    class RecipeStepConfiguration : IEntityTypeConfiguration<RecipeStep>
+    {
+        public void Configure(EntityTypeBuilder<RecipeStep> builder)
+        {
+            builder.HasOne(rs => rs.Recipe)
+                 .WithMany(r => r.RecipeSteps);
+
+            builder.Property(rs => rs.Instruction)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(rs => rs.ImageUrl)
+                .HasMaxLength(100);
+        }
+    }
+}
