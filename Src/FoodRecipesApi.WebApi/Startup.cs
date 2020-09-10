7f73796d11790871880003108311f427fad478e6
Daniel Ras <ras98.daniel@gmail.com>
2020-09-11 00:39:41 +0300

    feat: call AddPersistance method in ConfigureServices for Injection of Persistence project classes

--- Src/FoodRecipesApi.WebApi/Startup.cs
+++ Src/FoodRecipesApi.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodRecipesApi.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace FoodRecipesApi.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddPersistence(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
