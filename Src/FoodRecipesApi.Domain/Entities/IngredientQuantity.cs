8ebb3275406b8b58578e4c922e0527ce398b96b5
Daniel Ras <ras98.daniel@gmail.com>
2020-09-12 01:24:15 +0300

    feat: update IngredientQuantity MeasurementUnit prop to match MasurementUnit entity model

--- Src/FoodRecipesApi.Domain/Entities/IngredientQuantity.cs
+++ Src/FoodRecipesApi.Domain/Entities/IngredientQuantity.cs
@@ -8,7 +8,8 @@ namespace FoodRecipesApi.Domain.Entities
     {
         public int IngredientQuantityId { get; set; }
         public float Amount { get; set; }
-        public string MeasurementUnit { get; set; }
+        public MeasurementUnit MeasurementUnit { get; set; }
+        public int MeasurementUnitId { get; set; }
         public Ingredient Ingredient { get; set; }
         public int IngredientId { get; set; }
     }
