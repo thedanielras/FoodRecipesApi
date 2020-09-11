a6e95042bd15a87b04d1c60049b18200348dde96
Daniel Ras <ras98.daniel@gmail.com>
2020-09-12 01:36:11 +0300

    feat: add IngredientDto

---
+++ Src/FoodRecipesApi.Core/Common/Dtos/IngredientDto.cs
@@ -0,0 +1,14 @@
+﻿using FoodRecipesApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Common.Dtos
+{
+    public class IngredientDto
+    {
+        public int IngredientId { get; set; }
+        public string Name { get; set; }
+        public IngredientQuantityDto Quantity { get; set; }
+    }
+}
