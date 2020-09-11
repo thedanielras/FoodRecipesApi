dbe60eefe2c9b3740b33bc0413dea74cbf4712b0
Daniel Ras <ras98.daniel@gmail.com>
2020-09-12 01:35:52 +0300

    feat: add RecipeDto

---
+++ Src/FoodRecipesApi.Core/Common/Dtos/RecipeDto.cs
@@ -0,0 +1,21 @@
+﻿using FoodRecipesApi.Application.Common.Dtos;
+using FoodRecipesApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Common.Dtos
+{
+    public class RecipeDto
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string ImageUrl { get; set; }
+        public int PreparationTimeInMinutes { get; set; }
+        public int TotalTimeInMinutes { get; set; }
+        public IEnumerable<RecipeStep> RecipeSteps { get; set; }  
+        public IEnumerable<IngredientDto> RecipeIngredients { get; set; }
+    }
+}
