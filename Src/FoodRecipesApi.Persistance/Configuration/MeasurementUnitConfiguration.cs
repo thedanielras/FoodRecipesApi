3e6a707258c598cb1480de0d73fed92a76d01a91
Daniel Ras <ras98.daniel@gmail.com>
2020-09-12 01:28:33 +0300

    feat: add MeasurementUnitConfiguration

---
+++ Src/FoodRecipesApi.Persistance/Configuration/MeasurementUnitConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using FoodRecipesApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Persistence.Configuration
+{
+    class MeasurementUnitConfiguration : IEntityTypeConfiguration<MeasurementUnit>
+    {
+        public void Configure(EntityTypeBuilder<MeasurementUnit> builder)
+        {
+            builder.HasOne(mu => mu.IngredientQuantity)
+                .WithOne(iq => iq.MeasurementUnit)
+                .HasForeignKey<MeasurementUnit>(mu => mu.IngredientQuantityId);
+
+            builder.Property(mu => mu.Unit)
+                .HasMaxLength(15)
+                .IsRequired();
+        }
+    }
+}
