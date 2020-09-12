b05f1e93971b19134fcb7293b1cd26c3fd6c14ad
Daniel Ras <ras98.daniel@gmail.com>
2020-09-12 14:26:07 +0300

    feat: add GetAllRecipesQuery Metiatr request

---
+++ Src/FoodRecipesApi.Core/Common/Recipes/Queries/GetAllRecipes/GetAllRecipesQuery.cs
@@ -0,0 +1,11 @@
+﻿using FoodRecipesApi.Application.Common.Dtos;
+using FoodRecipesApi.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Common.Recipes.Queries.GetAllRecipes
+{
+    public class GetAllRecipesQuery : IRequest<List<RecipeDto>>{}
+}
