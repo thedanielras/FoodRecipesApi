5a96f45d6d6f46b4a9e2e245d6d8693fe14be272
Daniel Ras <ras98.daniel@gmail.com>
2020-09-12 23:54:00 +0300

    feat: add logging to GetAllRecipesQueryHandler

--- Src/FoodRecipesApi.Core/Common/Recipes/Queries/GetAllRecipes/GetAllRecipesQueryHandler.cs
+++ Src/FoodRecipesApi.Core/Common/Recipes/Queries/GetAllRecipes/GetAllRecipesQueryHandler.cs
@@ -3,6 +3,7 @@ using FoodRecipesApi.Application.Common.Interfaces;
 using FoodRecipesApi.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,45 +16,59 @@ namespace FoodRecipesApi.Application.Common.Recipes.Queries.GetAllRecipes
     public class GetAllRecipesQueryHandler : IRequestHandler<GetAllRecipesQuery, List<RecipeDto>>
     {
         private readonly IFoodRecipesDbContext _context;
+        private readonly ILogger<GetAllRecipesQueryHandler> _logger;
 
-        public GetAllRecipesQueryHandler(IFoodRecipesDbContext context)
+        public GetAllRecipesQueryHandler(IFoodRecipesDbContext context, ILogger<GetAllRecipesQueryHandler> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public Task<List<RecipeDto>> Handle(GetAllRecipesQuery request, CancellationToken cancellationToken)
         {
-            var recipes = _context.Recipes
-                 .Include(r => r.Author)
-                 .Include(r => r.RecipeIngredients)
-                     .ThenInclude(ri => ri.Ingredient)
-                         .ThenInclude(i => i.Quantity)
-                             .ThenInclude(q => q.MeasurementUnit)
-                 .Include(r => r.RecipeSteps)
-                 .Select(r => new RecipeDto()
-                 {
-                     RecipeId = r.RecipeId,
-                     Title = r.Title,
-                     Description = r.Description,
-                     Author = $"{r.Author.Name} {r.Author.Surname}",
-                     ImageUrl = r.ImageUrl,
-                     PreparationTimeInMinutes = r.PreparationTimeInMinutes,
-                     TotalTimeInMinutes = r.TotalTimeInMinutes,
-                     RecipeSteps = r.RecipeSteps,
-                     RecipeIngredients = r.RecipeIngredients.Select(ri => new IngredientDto()
-                     {
-                         IngredientId = ri.Ingredient.IngredientId,
-                         Name = ri.Ingredient.Name,
-                         Quantity = new IngredientQuantityDto()
-                         {
-                             Amount = ri.Ingredient.Quantity.Amount,
-                             MeasurementUnit = ri.Ingredient.Quantity.MeasurementUnit
-                         }
-                     })
-                 });
+            IQueryable<RecipeDto> recipes = null;
+
+            try
+            {
+                recipes = _context.Recipes
+                .Include(r => r.Author)
+                .Include(r => r.RecipeIngredients)
+                    .ThenInclude(ri => ri.Ingredient)
+                        .ThenInclude(i => i.Quantity)
+                            .ThenInclude(q => q.MeasurementUnit)
+                .Include(r => r.RecipeSteps)
+                .Select(r => new RecipeDto()
+                {
+                    RecipeId = r.RecipeId,
+                    Title = r.Title,
+                    Description = r.Description,
+                    Author = $"{r.Author.Name} {r.Author.Surname}",
+                    ImageUrl = r.ImageUrl,
+                    PreparationTimeInMinutes = r.PreparationTimeInMinutes,
+                    TotalTimeInMinutes = r.TotalTimeInMinutes,
+                    RecipeSteps = r.RecipeSteps.Select(rs => new RecipeStepDto() {
+                        RecipeStepId = rs.RecipeStepId,
+                        Instruction = rs.Instruction,
+                        ImageUrl = rs.ImageUrl
+                    }),
+                    RecipeIngredients = r.RecipeIngredients.Select(ri => new IngredientDto()
+                    {
+                        IngredientId = ri.Ingredient.IngredientId,
+                        Name = ri.Ingredient.Name,
+                        Quantity = new IngredientQuantityDto()
+                        {
+                            Amount = ri.Ingredient.Quantity.Amount,
+                            MeasurementUnit = ri.Ingredient.Quantity.MeasurementUnit.Unit
+                        }
+                    })
+                });
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
 
 
-            return recipes.ToListAsync<RecipeDto>();
+            return recipes.ToListAsync<RecipeDto>() ?? null;
         }
     }
 }
