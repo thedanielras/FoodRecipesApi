20a4cf5f02b3c52f1f330745baef8205887c2ec0
Daniel Ras <ras98.daniel@gmail.com>
2020-09-13 16:17:18 +0300

    feat: override SaveChangeAsync base method

--- Src/FoodRecipesApi.Persistance/FoodRecipesDbContext.cs
+++ Src/FoodRecipesApi.Persistance/FoodRecipesDbContext.cs
@@ -4,12 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FoodRecipesApi.Persistence
 {
     public class FoodRecipesDbContext : DbContext, IFoodRecipesDbContext
     {
-        public FoodRecipesDbContext(DbContextOptions<FoodRecipesDbContext> options) 
+        public FoodRecipesDbContext(DbContextOptions<FoodRecipesDbContext> options)
             : base(options)
         {
         }
@@ -20,6 +22,11 @@ namespace FoodRecipesApi.Persistence
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(FoodRecipesDbContext).Assembly);
+        }
+
+        public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync();
         }
     }
 }
