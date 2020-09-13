90a87913a39dd32e3ba226b2bd9d9d694536206a
Daniel Ras <ras98.daniel@gmail.com>
2020-09-13 16:02:13 +0300

    feat: add IngredientQuantityDto

---
+++ Src/FoodRecipesApi.Core/Common/Dtos/IngredientQuantityCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using FoodRecipesApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Common.Dtos
+{
+    public class IngredientQuantityCreateDto
+    {
+        public float Amount { get; set; }
+        public MeasurementUnitCreateDto MeasurementUnit { get; set; }
+    }
+}
