224c1c78473a746c75aa22e2178473fa875e946c
Daniel Ras <ras98.daniel@gmail.com>
2020-09-14 01:18:25 +0300

    feat: add AddAlternateKeyByRecipeAuthorIdAndTitleProps migration

--- Src/FoodRecipesApi.Persistance/Migrations/20200913211251_AddAlternateKeyByRecipeAuthorIdAndTitleProps.Designer.cs
+++ Src/FoodRecipesApi.Persistance/Migrations/20200913211251_AddAlternateKeyByRecipeAuthorIdAndTitleProps.Designer.cs
@@ -3,14 +3,16 @@ using FoodRecipesApi.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FoodRecipesApi.Persistence.Migrations
 {
     [DbContext(typeof(FoodRecipesDbContext))]
-    partial class FoodRecipesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200913211251_AddAlternateKeyByRecipeAuthorIdAndTitleProps")]
+    partial class AddAlternateKeyByRecipeAuthorIdAndTitleProps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
