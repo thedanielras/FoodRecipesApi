5ef830e524f46fb3a4e0d7df8a30df6bd6c4831f
Daniel Ras <ras98.daniel@gmail.com>
2020-09-14 01:19:36 +0300

    feat: set up DB migration and seeding

--- Src/FoodRecipesApi.WebApi/Program.cs
+++ Src/FoodRecipesApi.WebApi/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodRecipesApi.Application.System.Commands.DbSeedCommand;
+using FoodRecipesApi.Persistence;
+using MediatR;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +16,30 @@ namespace FoodRecipesApi.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostBuilder = CreateHostBuilder(args).Build();
+
+            using (var scope = hostBuilder.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;               
+
+                try
+                {
+                    var foodRecipesContext = serviceProvider.GetRequiredService<FoodRecipesDbContext>();
+                    foodRecipesContext.Database.Migrate();
+
+                    var mediator = serviceProvider.GetRequiredService<IMediator>();
+                    await mediator.Send(new DbSeedCommand());
+                }
+                catch(Exception ex)
+                {
+                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occurred while initializing the database");
+                }              
+            }
+
+            hostBuilder.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
