8b22c411cecca9114808d752a07d6ff85762da6b
Daniel Ras <ras98.daniel@gmail.com>
2020-09-13 16:16:47 +0300

    feat: update Recipe Entity

--- Src/FoodRecipesApi.Domain/Entities/Recipe.cs
+++ Src/FoodRecipesApi.Domain/Entities/Recipe.cs
@@ -18,10 +18,10 @@ namespace FoodRecipesApi.Domain.Entities
         public string Description { get; set; }
         public Author Author { get; set; }
         public int AuthorId { get; set; }
-        public ICollection<RecipeStep> RecipeSteps { get; private set; }
+        public ICollection<RecipeStep> RecipeSteps { get; set; }
         public string ImageUrl { get; set; }
         public int PreparationTimeInMinutes { get; set; }
         public int TotalTimeInMinutes { get; set; }
-        public ICollection<RecipeIngredient> RecipeIngredients { get; private set; }
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; }
     }
 }
