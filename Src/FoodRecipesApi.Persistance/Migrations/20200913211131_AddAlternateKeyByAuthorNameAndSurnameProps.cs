fcd46f9701a4d10de6d1ea4af5b9512825604b3a
Daniel Ras <ras98.daniel@gmail.com>
2020-09-14 01:17:49 +0300

    feat: add AddAlternateKeyByAuthorNameAndSurnameProps migration

---
+++ Src/FoodRecipesApi.Persistance/Migrations/20200913211131_AddAlternateKeyByAuthorNameAndSurnameProps.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodRecipesApi.Persistence.Migrations
+{
+    public partial class AddAlternateKeyByAuthorNameAndSurnameProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Authors_Name_Surname",
+                table: "Authors",
+                columns: new[] { "Name", "Surname" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Authors_Name_Surname",
+                table: "Authors");
+        }
+    }
+}
