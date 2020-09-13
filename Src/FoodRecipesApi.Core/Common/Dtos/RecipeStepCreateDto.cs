a2acfa3b6dc4e8cdfffb02c360c0905aa5e6e244
Daniel Ras <ras98.daniel@gmail.com>
2020-09-13 16:02:57 +0300

    feat: add RecipeStepCreateDto

---
+++ Src/FoodRecipesApi.Core/Common/Dtos/RecipeStepCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Common.Dtos
+{
+    public class RecipeStepCreateDto
+    {
+        public string Instruction { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
