ab30a56ca9e3097a8639fdaf7b389ec52aaa001a
Daniel Ras <ras98.daniel@gmail.com>
2020-09-13 16:18:23 +0300

    feat: add getById/create/delete controller actions

--- Src/FoodRecipesApi.WebApi/Controllers/FoodRecipesController.cs
+++ Src/FoodRecipesApi.WebApi/Controllers/FoodRecipesController.cs
@@ -1,6 +1,9 @@
 ﻿using FoodRecipesApi.Application.Common.Dtos;
 using FoodRecipesApi.Application.Common.Interfaces;
-using FoodRecipesApi.Application.Common.Recipes.Queries.GetAllRecipes;
+using FoodRecipesApi.Application.Recipes.Commands.CreateRecipe;
+using FoodRecipesApi.Application.Recipes.Commands.DeleteRecipe;
+using FoodRecipesApi.Application.Recipes.Queries.GetAllRecipes;
+using FoodRecipesApi.Application.Recipes.Queries.GetRecipe;
 using FoodRecipesApi.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -25,11 +28,39 @@ namespace FoodRecipesApi.WebApi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<RecipeDto>>> Get()
+        public async Task<ActionResult<IEnumerable<RecipeDto>>> GetAllRecipes()
         {
-           var recipes = await _mediator.Send(new GetAllRecipesQuery());
+            var recipes = await _mediator.Send(new GetAllRecipesQuery());
 
             return Ok(recipes);
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<RecipeDto>> GetRecipeById(int id)
+        {
+            var recipe = await _mediator.Send(new GetRecipeQuery(id));
+
+
+            if (recipe == null)
+                return NotFound();
+            else
+                return Ok(recipe);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateRecipe(CreateRecipeCommand createRecipeCommand)
+        {
+            int recipeId = await _mediator.Send(createRecipeCommand);
+
+            return Ok(recipeId);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteRecipe(int id)
+        {
+            await _mediator.Send(new DeleteRecipeCommand(id));
+
+            return NoContent();
+        } 
     }
 }
