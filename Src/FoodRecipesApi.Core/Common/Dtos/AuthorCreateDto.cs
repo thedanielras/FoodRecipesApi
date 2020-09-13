61c56a620ba40be6be4c31725194ce9fd7c92980
Daniel Ras <ras98.daniel@gmail.com>
2020-09-13 16:01:39 +0300

    feat: add AuthorCreateDto

---
+++ Src/FoodRecipesApi.Core/Common/Dtos/AuthorCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Common.Dtos
+{
+    public class AuthorCreateDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Born { get; set; }
+    }
+}
