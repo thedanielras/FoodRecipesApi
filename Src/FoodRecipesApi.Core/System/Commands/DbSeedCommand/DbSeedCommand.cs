f5087163da9d613dfb03d7ab6a52893e84176d4f
Daniel Ras <ras98.daniel@gmail.com>
2020-09-14 01:15:40 +0300

    feat: add DbSeed MediatR command

---
+++ Src/FoodRecipesApi.Core/System/Commands/DbSeedCommand/DbSeedCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.System.Commands.DbSeedCommand
+{
+    public class DbSeedCommand : IRequest<Unit>
+    {
+    }
+}
