45850ebdbaa8be9a69a3b716c8d2345d291b8915
Daniel Ras <ras98.daniel@gmail.com>
2020-09-14 23:52:40 +0300

    feat: init NSwag package to server SwaggerUI

--- Src/FoodRecipesApi.WebApi/Startup.cs
+++ Src/FoodRecipesApi.WebApi/Startup.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
 using FoodRecipesApi.Application;
+using FoodRecipesApi.Application.Common.Interfaces;
 using FoodRecipesApi.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,7 +32,27 @@ namespace FoodRecipesApi.WebApi
         {
             services.AddPersistence(Configuration);
             services.AddApplication();
-            services.AddControllers();            
+            services.AddControllers();
+            services.AddSwaggerDocument(confing => 
+            confing.PostProcess = document => 
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "FoodRecipes API";
+                    document.Info.Description = "An API that serves food recipes";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Daniel Ras",
+                        Email = "ras98.daniel@gmail.com",
+                        Url = "https://thedanielras.github.io/"
+                    };
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = "https://opensource.org/licenses/MIT"
+                    };
+                }
+            );            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +68,9 @@ namespace FoodRecipesApi.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseEndpoints(endpoints =>
             {
