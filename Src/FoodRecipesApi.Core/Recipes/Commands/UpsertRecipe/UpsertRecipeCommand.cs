a2e0040fc9f76d423a9c77198630dfffe36add02
Daniel Ras <ras98.daniel@gmail.com>
2020-09-21 11:17:02 +0300

    feat: add UpserRecipeCommand Mediatr request

---
+++ Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/UpsertRecipeCommand.cs
@@ -0,0 +1,22 @@
+﻿using FoodRecipesApi.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Recipes.Commands.UpsertRecipe
+{
+    public class UpsertRecipeCommand : IRequest<int>
+    {
+        public int? RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public AuthorCreateDto Author { get; set; }
+        public int AuthorId { get; set; }
+        public List<RecipeStepCreateDto> RecipeSteps { get; set; }
+        public string ImageUrl { get; set; }
+        public int PreparationTimeInMinutes { get; set; }
+        public int TotalTimeInMinutes { get; set; }
+        public List<RecipeIngredientCreateDto> RecipeIngredients { get; set; }
+    }
+}
