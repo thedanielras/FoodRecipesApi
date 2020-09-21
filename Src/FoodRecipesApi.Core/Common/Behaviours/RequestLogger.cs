edb04880cb7131c9f8951fd91a0e2b2baa8ecc5f
Daniel Ras <ras98.daniel@gmail.com>
2020-09-21 21:34:01 +0300

    feat: set up logging Mediatr Request behavior

---
+++ Src/FoodRecipesApi.Core/Common/Behaviours/RequestLogger.cs
@@ -0,0 +1,29 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.Application.Common.Behaviours
+{
+    class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public RequestLogger(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var name = typeof(TRequest).Name;
+            _logger.LogWarning("Food Recipes API Request: {Name} {@Request}",
+               name, request);
+
+            return Task.CompletedTask;
+        }
+    }
+}
