f66ac81f892f51319b329431165e8f96ba3f66d0
Daniel Ras <ras98.daniel@gmail.com>
2020-09-21 11:29:28 +0300

    feat: add BaseController abstract class that implements custom error handler "SendError"

---
+++ Src/FoodRecipesApi.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using FoodRecipesApi.WebApi.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {        
+        public virtual ActionResult<ApiErrorModel> SendError()
+        {
+            int statusCode = StatusCodes.Status500InternalServerError;
+            var exceptionHandler = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            var error = new ApiErrorModel(statusCode, exceptionHandler.Error.Message);
+
+            return StatusCode(statusCode, error);
+        }
+
+    }
+}
