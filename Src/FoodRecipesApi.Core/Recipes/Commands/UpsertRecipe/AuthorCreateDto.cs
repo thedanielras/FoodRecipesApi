a749fb46963fe84d8ad89646eb1af2dc66ea8c0b
Daniel Ras <ras98.daniel@gmail.com>
2020-09-21 11:17:56 +0300

    feat: add AuthorCreateDto

---
+++ Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/AuthorCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Recipes.Commands.UpsertRecipe
+{
+    public class AuthorCreateDto
+    {
+        public int? AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
