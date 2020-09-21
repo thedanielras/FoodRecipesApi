20cd7b279bb936a9499357caf88f6166716e974e
Daniel Ras <ras98.daniel@gmail.com>
2020-09-21 11:29:43 +0300

    feat: add ApiErrorModel class

---
+++ Src/FoodRecipesApi.WebApi/Models/ApiErrorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.WebApi.Models
+{
+    public class ApiErrorModel
+    {       
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public ApiErrorModel(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
