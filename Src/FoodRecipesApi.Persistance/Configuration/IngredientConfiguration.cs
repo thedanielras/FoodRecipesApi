d798d8440a316860fdcc51f7084a0d9d0ee1778b
Daniel Ras <ras98.daniel@gmail.com>
2020-09-21 11:26:48 +0300

    feat: update IngredientConfiguration

--- Src/FoodRecipesApi.Persistance/Configuration/IngredientConfiguration.cs
+++ Src/FoodRecipesApi.Persistance/Configuration/IngredientConfiguration.cs
@@ -10,10 +10,11 @@ namespace FoodRecipesApi.Persistence.Configuration
     class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
     {
         public void Configure(EntityTypeBuilder<Ingredient> builder)
-        {
+        {           
             builder.HasOne(i => i.Quantity)
                .WithOne(q => q.Ingredient)
                .HasForeignKey<Ingredient>(i => i.QuantityId)
+               .OnDelete(DeleteBehavior.Cascade)
                .IsRequired();
 
             builder.Property(i => i.Name)
