0fe046f52bd46555dabbcf04aea019affb656542
Daniel Ras <ras98.daniel@gmail.com>
2020-09-22 10:28:29 +0300

    feat: update BaseController to wrap exceptions in BaseJson

--- Src/FoodRecipesApi.WebApi/Controllers/BaseController.cs
+++ Src/FoodRecipesApi.WebApi/Controllers/BaseController.cs
@@ -1,24 +1,41 @@
-﻿using FoodRecipesApi.WebApi.Models;
+﻿using FoodRecipesApi.Application.Common.Exceptions;
+using FoodRecipesApi.WebApi.Models;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace FoodRecipesApi.WebApi.Controllers
 {
     public abstract class BaseController : ControllerBase
     {        
-        public virtual ActionResult<ApiErrorModel> SendError()
+        public virtual ActionResult SendError()
         {
-            int statusCode = StatusCodes.Status500InternalServerError;
-            var exceptionHandler = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
 
-            var error = new ApiErrorModel(statusCode, exceptionHandler.Error.Message);
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            string resultMessage = exception.Message;
+            object result = null;                
 
-            return StatusCode(statusCode, error);
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    statusCode = HttpStatusCode.BadRequest;                
+                    result = validationException.Failures;
+                    break;
+                case NotFoundException _:
+                    statusCode = HttpStatusCode.NotFound;
+                    break;
+            }
+
+            BaseJson resultJson = new BaseJson((int)statusCode, resultMessage, result);
+
+            return new ObjectResult(resultJson) { StatusCode = (int)statusCode};
         }
 
     }
