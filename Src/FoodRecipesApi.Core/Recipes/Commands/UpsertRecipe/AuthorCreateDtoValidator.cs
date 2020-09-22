accded32a1d908074be25c8229201863328d87be
Daniel Ras <ras98.daniel@gmail.com>
2020-09-22 10:57:15 +0300

    feat: update AuthorCreateDtoValidator to add Email validation

--- Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/AuthorCreateDtoValidator.cs
+++ Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/AuthorCreateDtoValidator.cs
@@ -13,6 +13,9 @@ namespace FoodRecipesApi.Application.Recipes.Commands.UpsertRecipe
             RuleFor(a => a.Name).MaximumLength(50);
             RuleFor(a => a.Surname).NotEmpty();
             RuleFor(a => a.Surname).MaximumLength(50);
+            RuleFor(a => a.Email).NotEmpty();
+            RuleFor(a => a.Email).MaximumLength(60);
+            RuleFor(a => a.Email).EmailAddress();   
             RuleFor(a => a.ImageUrl).MaximumLength(250);
         }
     }
