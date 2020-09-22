ee7362f2fbe03acb3a925da2396bab6fcbb7cda9
Daniel Ras <ras98.daniel@gmail.com>
2020-09-22 10:24:31 +0300

    feat: add RecipeStepCreateDtoValidator

---
+++ Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/RecipeStepCreateDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Application.Recipes.Commands.UpsertRecipe
+{
+    public class RecipeStepCreateDtoValidator : AbstractValidator<RecipeStepCreateDto>
+    {
+        public RecipeStepCreateDtoValidator()
+        {
+            RuleFor(rs => rs.Instruction).NotEmpty();
+            RuleFor(rs => rs.Instruction).MaximumLength(500);
+            RuleFor(rs => rs.ImageUrl).MaximumLength(250);
+        }
+    }
+}
