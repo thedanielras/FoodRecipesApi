892207d6bc8a65007397d2e91df0dff5854baeb7
Daniel Ras <ras98.daniel@gmail.com>
2020-09-22 10:55:22 +0300

    feat: add Email prop

--- Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/AuthorCreateDto.cs
+++ Src/FoodRecipesApi.Core/Recipes/Commands/UpsertRecipe/AuthorCreateDto.cs
@@ -9,6 +9,7 @@ namespace FoodRecipesApi.Application.Recipes.Commands.UpsertRecipe
         public int? AuthorId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Email { get; set; }
         public string ImageUrl { get; set; }
     }
 }
