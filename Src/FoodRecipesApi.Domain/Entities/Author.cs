e1a8d285049df04d9daf45c33d931371a0435650
Daniel Ras <ras98.daniel@gmail.com>
2020-09-22 10:54:02 +0300

    feat: add EmailAdress prop to Author entity

--- Src/FoodRecipesApi.Domain/Entities/Author.cs
+++ Src/FoodRecipesApi.Domain/Entities/Author.cs
@@ -14,6 +14,7 @@ namespace FoodRecipesApi.Domain.Entities
         public int AuthorId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string EmailAdress { get; set; }
         public string ImageUrl { get; set; }
         public ICollection<Recipe> Recipes { get; private set; }
     }
